d846094a6b01bb364f838f29f34c462e61496bc8
Nick Fox-Gieg <nick@fox-gieg.com>
2025-11-24 11:37:34 -0600

    Update generate_password.sh

--- generate_password.sh
+++ generate_password.sh
@@ -1,1 +1,6 @@
-htpasswd -c /home/username/example.com/.htpasswd user1
+AUTH_LINE=$(grep -i "^AuthUserFile" ../.htaccess)
+FILE_PATH=$(echo "$AUTH_LINE" | awk '{print $2}')
+USER_NAME=$(echo "$FILE_PATH" | awk -F'/' '{print $3}')
+
+#htpasswd -c /home/username/example.com/.htpasswd user1
+htpasswd -c $FILE_PATH $USER_NAME
