b5bd278920d49eeb5cd5603a820746a9bcf28410
Nick Fox-Gieg <nick@fox-gieg.com>
2025-11-24 11:42:37 -0600

    Update generate_password.sh

--- generate_password.sh
+++ generate_password.sh
@@ -3,7 +3,7 @@ FILE_PATH=$(echo "$AUTH_LINE" | awk '{print $2}')
 USER_NAME=$(echo "$FILE_PATH" | awk -F'/' '{print $3}')
 
 #htpasswd -c /home/username/example.com/.htpasswd user1
-CMD_LINE = htpasswd -c $FILE_PATH $USER_NAME
-echo $CMD_LINE
-$(echo $CMD_LINE)
+NEW_CMD="htpasswd -c $FILE_PATH $USER_NAME"
+echo $NEW_CMD
+$(echo $NEW_CMD)
 
